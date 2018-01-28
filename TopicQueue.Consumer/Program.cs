0821d7f11955e572d866997d98c2d7ea1bb1624e
bchornii <bogdan.chorniy@gmail.com>
2018-01-28 23:22:02 +0200

    Added topic queue.

---
+++ TopicQueue.Consumer/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace TopicQueue.Consumer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory {HostName = "localhost"};
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    // Declare exchange
+                    channel.ExchangeDeclare(
+                        exchange: "topic_logs",
+                        type: "topic");
+
+                    var queueName = channel.QueueDeclare().QueueName;
+
+                    // Create queue with auto-generated name, dureless,
+                    // which will be deleted after consumer down
+                    if (args.Length < 1)
+                    {
+                        Console.Error.WriteLine("Usage: {0} [binding_key...]",
+                            Environment.GetCommandLineArgs()[0]);
+                        Console.WriteLine(" Press [enter] to exit.");
+                        Console.ReadLine();
+                        Environment.ExitCode = 1;
+                        return;
+                    }
+
+                    foreach (var routingKey in args)
+                    {
+                        channel.QueueBind(
+                            queue: queueName,
+                            exchange: "topic_logs",
+                            routingKey: routingKey);
+                    }
+
+                    Console.WriteLine(" [*] Waiting for messages. To exit press CTRL+C");
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        var routingKey = ea.RoutingKey;
+                        Console.WriteLine(" [x] Received '{0}':'{1}'", routingKey, message);
+                    };
+
+                    // Consume messages
+                    channel.BasicConsume(
+                        queue: queueName,                        
+                        autoAck: true,
+                        consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ TopicQueue.Producer/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+
+/* 
+ * In our logging system we might want to subscribe to not 
+ * only logs based on severity, but also based on the 
+ * source which emitted the log.
+ * 
+ * Messages sent to a topic exchange can't have an arbitrary 
+ * routing_key - it must be a list of words, delimited by dots.
+ * for ex.: "stock.usd.nyse", "nyse.vmw", "quick.orange.rabbit"
+ * 
+ * The logic behind the topic exchange is similar to a direct one - 
+ * a message sent with a particular routing key will be delivered 
+ * to all the queues that are bound with a matching binding key (routingKey parameter).
+ * 
+ * Two important special cases for binding keys:
+ *  - * (star) can substitute for exactly one word
+ *  - # (hash) can substitute for zero or more words
+ * 
+ * When a queue is bound with "#" (hash) binding key - it will 
+ * receive all the messages, regardless of the routing key - 
+ * like in fanout exchange.
+ * 
+ * When special characters "*" (star) and "#" (hash) aren't 
+ * used in bindings, the topic exchange will behave just like 
+ * a direct one.
+ */
+
+namespace TopicQueue.Producer
+{    
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory {HostName = "localhost"};
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    // Declare exchange
+                    channel.ExchangeDeclare(
+                        exchange: "topic_logs",
+                        type: "topic");
+
+                    var routingKey = GetRoutingKey(args);
+                    var message = GetMessage(args);
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    // Publish message to exhange
+                    channel.BasicPublish(
+                        exchange: "topic_logs",
+                        routingKey: routingKey,
+                        basicProperties: null,
+                        body: body);
+
+                    Console.WriteLine(" [x] Sent '{0}':'{1}'", routingKey, message);
+                }
+            }
+        }
+
+        private static string GetMessage(string[] args) => 
+            args.Length > 1 ? string.Join(" ", args.Skip(1)) : "Hello World!";
+
+        private static string GetRoutingKey(string[] args) => 
+            args.Length > 0 ? args[0] : "anonymous.info";
+    }
+}
