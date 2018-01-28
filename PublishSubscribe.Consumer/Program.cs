1e872c1c89364ccaa8739414f24898fa8715c1f7
bchornii <bogdan.chorniy@gmail.com>
2018-01-28 19:45:20 +0200

    Added publish/subscribe.

---
+++ PublishSubscribe.Consumer/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace PublishSubscribe.Consumer
+{
+    // We want to hear about all log messages, not just a subset of them.
+    // We're also interested only in currently flowing messages not in the old ones.
+    // To solve that we need two things:
+    // - Whenever we connect to Rabbit we need a fresh, empty queue.
+    //   To do this we could create a queue with a random name, or, 
+    //   even better - let the server choose a random queue name for us.
+    // - Once we disconnect the consumer the queue should be automatically deleted
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(
+                        exchange: "logs", 
+                        type: "fanout");
+
+                    // When we supply no parameters to QueueDeclare() we create a 
+                    // non-durable, exclusive, autodelete queue with a auto-generated name.
+                    var queueName = channel.QueueDeclare().QueueName;
+
+                    // We've already created a fanout exchange and a queue. 
+                    // Now we need to tell the exchange to send messages to our queue. 
+                    // That relationship between exchange and a queue is called a binding.
+                    channel.QueueBind(
+                        queue: queueName,
+                        exchange: "logs",
+                        routingKey: "");
+
+                    Console.WriteLine(" [*] Waiting for logs.");
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] {0}", message);
+                    };
+                    channel.BasicConsume(
+                        queue: queueName,
+                        autoAck: true,
+                        consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+
+                }
+            }
+        }
+    }
+}
---
+++ PublishSubscribe.Producer/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace PublishSubscribe.Producer
+{
+    // The core idea in the messaging model in RabbitMQ is that the 
+    // producer never sends any messages directly to a queue. 
+    // Actually, quite often the producer doesn't even know if a message 
+    // will be delivered to any queue at all.
+    // Instead, the producer can only send messages to an exchange.
+    // Exchange on one side it receives messages from producers 
+    // and the other side it pushes them to queues.
+
+    // Exchange types: direct, topic, headers and fanout.
+    // fanout exchange broadcasts all the messages it receives to all the queues it knows.
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    // The messages will be lost if no queue is bound to the exchange yet, 
+                    // but that's okay for us; if no consumer is listening yet we can 
+                    // safely discard the message.
+                    channel.ExchangeDeclare(
+                        exchange: "logs", 
+                        type: "fanout");
+
+                    var message = GetMessage(args);
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(
+                        exchange: "logs",
+                        routingKey: "",
+                        basicProperties: null,
+                        body: body);
+
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return args.Length > 0 ? string.Join(" ", args) : "info: Hello World!";
+        }
+    }
+}
